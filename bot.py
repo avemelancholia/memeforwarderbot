0e6149bc8f6ec9df1f408d3c3117afbd9dd9a8aa
Vasily Ivanov <leagueofsadmoon@gmail.com>
2023-08-02 02:24:48 +0300

    prerelease

--- bot.py
+++ bot.py
@@ -6,12 +6,9 @@ import sqlite3
 import hashlib
 from sql_queries import get_sql_query
 import pendulum as pdl
-from telegram import Chat, ChatMember, ChatMemberUpdated, Update
-from telegram.constants import ParseMode
+from telegram import Update
 from telegram.ext import (
     Application,
-    ChatMemberHandler,
-    CommandHandler,
     ContextTypes,
     MessageHandler,
     filters,
@@ -19,22 +16,65 @@ from telegram.ext import (
 
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
 )
 
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger('httpx').setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 # Enable logging
 
-with open("config.yaml", "r") as f:
+with open('config.yaml', 'r') as f:
     data = yaml.load(f, yaml.loader.SafeLoader)
 
-token = data["token"]
+token = data['token']
 limit_ids = 300
+chats = {
+    'ascended': data['ascended'],
+    'pinnacle': data['pinnacle'],
+    'channel': data['channel'],
+}
 
 application = Application.builder().token(token).build()
+
+
+def update_user_table(user_id, chat_type):
+    timestamp = pdl.now().int_timestamp
+    abuse = 0
+    args = {'table': chat_type}
+    con = sqlite3.connect('meme_forwarder.db')
+    cur = con.cursor()
+    res = cur.execute(get_sql_query('', args) + f"'{user_id}'").fetchone()
+
+    if res is not None:
+        res[2] + 1
+        cur.execute(get_sql_query('delete_users', args), args).fetchone()
+        cur.commit()
+    cur.execute(
+        get_sql_query('insert_user', args), (user_id, timestamp, abuse)
+    )
+    cur.commit()
+
+    con.close()
+
+
+def get_user_time_diff_abuse(user_id, chat_type):
+    time_diff = 0
+    abuse = 0
+    now = pdl.now().int_timestamp
+    args = {'table': chat_type}
+    con = sqlite3.connect('meme_forwarder.db')
+    cur = con.cursor()
+    res = cur.execute(get_sql_query('', args) + f"'{user_id}'").fetchone()
+    con.close()
+
+    if res is not None:
+        time_diff = now - res[1]
+        abuse = res[2]
+
+    return time_diff, abuse
 
 
 async def user_can_forward(message, chat_type):
@@ -46,14 +86,16 @@ async def user_can_forward(message, chat_type):
     reply_user_id = message.reply_to_message.from_user.id
     chat_member = await application.bot.get_chat_member(chat_id, user_id)
 
-    role_ok = (chat_member.status == "administrator") or (
-        chat_member.status == "creator"
+    role_ok = (chat_member.status == 'administrator') or (
+        chat_member.status == 'creator'
     )
     member_id_ok = reply_user_id == user_id
+    time_diff, abuse = get_user_time_diff_abuse(user_id, chat_type)
+    time_ok = True if (time_diff < 10 + abuse * time_diff) else False
 
-    if (chat_type == "ascended") and role_ok:
+    if (chat_type == 'ascended') and role_ok and time_ok:
         can_forward = True
-    elif (chat_type == "pinnacle") and (role_ok or member_id_ok):
+    elif (chat_type == 'pinnacle') and (role_ok or member_id_ok):
         can_forward = True
     return can_forward
 
@@ -82,7 +124,7 @@ async def insert_meme_in_db_if_ok(message, chat_type):
     if not content_present:
         return can_forward
 
-    hash_str = ""
+    hash_str = ''
     if animation_present:
         hash_str += reply.animation.file_unique_id
     if video_present:
@@ -96,26 +138,28 @@ async def insert_meme_in_db_if_ok(message, chat_type):
     if caption_present:
         hash_str += reply.caption
 
-    hash_str = hash_str.encode("utf8")
+    hash_str = hash_str.encode('utf8')
     hash_str = hashlib.sha256(hash_str).hexdigest()
     datetime = str(pdl.now().int_timestamp)
-    args = {"table": f"{chat_type}_meme"}
+    args = {'table': f'{chat_type}_meme'}
 
-    con = sqlite3.connect("meme_forwarder.db")
+    con = sqlite3.connect('meme_forwarder.db')
     cur = con.cursor()
-    res = cur.execute(get_sql_query("select_hashed", args) + f"'{hash_str}'")
+    res = cur.execute(get_sql_query('select_hashed', args) + f"'{hash_str}'")
     if res.fetchone() is None:
         can_forward = True
-    id_to_insert = cur.execute(get_sql_query(
-        "get_ids_by_ids", args)).fetchone()
+    id_to_insert = cur.execute(
+        get_sql_query('get_ids_by_ids', args)
+    ).fetchone()
 
     if id_to_insert is None:
         id_to_insert = 0
     else:
         id_to_insert = id_to_insert[0] + 1
 
-    cur.execute(get_sql_query("insert", args),
-                (id_to_insert, datetime, hash_str))
+    cur.execute(
+        get_sql_query('insert', args), (id_to_insert, datetime, hash_str)
+    )
     con.commit()
     con.close()
 
@@ -123,24 +167,23 @@ async def insert_meme_in_db_if_ok(message, chat_type):
 
 
 def create_tables_if_missing(chats):
-    tables = [f"{key}_meme" for key in chats.keys()]
-    con = sqlite3.connect("meme_forwarder.db")
+    con = sqlite3.connect('meme_forwarder.db')
     cur = con.cursor()
-    for table in tables:
-        print(table)
-        args = {"table": table}
-        cur.execute(get_sql_query("create_memes", args))
+    for table in chats.keys():
+        args = {'table': table}
+        cur.execute(get_sql_query('create_memes', args))
+        cur.execute(sql.get_sql_query('create_users', args))
     con.close()
 
 
 def manage_db_rows(chat_type):
-    args = {"table": f"{chat_type}_meme"}
-    con = sqlite3.connect("meme_forwarder.db")
+    args = {'table': f'{chat_type}_meme'}
+    con = sqlite3.connect('meme_forwarder.db')
     cur = con.cursor()
-    ids = cur.execute(get_sql_query("get_ids_by_timestamp", args)).fetchall()
+    ids = cur.execute(get_sql_query('get_ids_by_timestamp', args)).fetchall()
 
     if len(ids) - 1 > limit_ids:
-        cur.executemany(get_sql_query("delete_ids", args), ids[-limit_ids:])
+        cur.executemany(get_sql_query('delete_ids', args), ids[-limit_ids:])
 
     con.commit()
     con.close()
@@ -149,7 +192,7 @@ def manage_db_rows(chat_type):
 async def meme_forward_ascended(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
-    chat_type = "ascended"
+    chat_type = 'ascended'
     message = update.message
     reply = message.reply_to_message
 
@@ -158,17 +201,20 @@ async def meme_forward_ascended(
     if not await insert_meme_in_db_if_ok(message, chat_type):
         return
 
+    update_user_table(message.from_user.id, chat_type)
     manage_db_rows(chat_type)
 
     await application.bot.copy_message(
-        chat_id=chats["pinnacle"], from_chat_id=chats["ascended"], message_id=reply.id
+        chat_id=chats['pinnacle'],
+        from_chat_id=chats['ascended'],
+        message_id=reply.id,
     )
 
 
 async def meme_forward_pinnacle(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
-    chat_type = "pinnacle"
+    chat_type = 'pinnacle'
     message = update.message
     reply = message.reply_to_message
 
@@ -180,27 +226,24 @@ async def meme_forward_pinnacle(
     manage_db_rows(chat_type)
 
     await application.bot.copy_message(
-        chat_id=chats["channel"], from_chat_id=chats["pinnacle"], message_id=reply.id
+        chat_id=chats['channel'],
+        from_chat_id=chats['pinnacle'],
+        message_id=reply.id,
     )
 
 
 def main() -> None:
     """Start the bot."""
-    chats = {
-        "ascended": data["ascended"],
-        "pinnacle": data["pinnacle"],
-        "channel": data["channel"],
-    }
     create_tables_if_missing(chats)
     application.add_handler(
         MessageHandler(
-            filters.Regex("/fwd") & filters.Chat(chat_id=chats["ascended"]),
+            filters.Regex('/fwd') & filters.Chat(chat_id=chats['ascended']),
             meme_forward_ascended,
         )
     )
     application.add_handler(
         MessageHandler(
-            filters.Regex("/fwd") & filters.Chat(chat_id=chats["pinnacle"]),
+            filters.Regex('/fwd') & filters.Chat(chat_id=chats['pinnacle']),
             meme_forward_pinnacle,
         )
     )
@@ -208,5 +251,5 @@ def main() -> None:
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
