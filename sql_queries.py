14144443749384a450d04bb167e82b952a1d927d
Vasily Ivanov <leagueofsadmoon@gmail.com>
2023-08-02 02:24:48 +0300

    prerelease

--- sql_queries.py
+++ sql_queries.py
@@ -1,24 +1,41 @@
 def get_sql_query(name, vars):
-    table = vars["table"]
+    table = vars['table']
     sql_create_memes_table = f"""
-    CREATE TABLE IF NOT EXISTS {table}(
+    CREATE TABLE IF NOT EXISTS {table}_meme(
         id INTEGER PRIMARY KEY,
         date TEXT,
         hash TEXT
         )
     """
-    sql_select_hashed = f"SELECT * from {table} WHERE {table}.hash = "
-    sql_get_ids_by_id = f"SELECT id from {table} ORDER BY id DESC"
-    sql_get_ids_by_ts = f"SELECT id from {table} ORDER BY date DESC"
-    sql_insert = f"INSERT INTO {table} VALUES (?, ?, ?)"
-    sql_delete_ids = f"DELETE from {table} WHERE id = ?"
+    sql_create_user_table = f"""CREATE TABLE IF NOT EXISTS {table}_user(
+        user_id INTEGER PRIMARY KEY,
+        date TEXT,
+        abuse, INTEGER
+        )
+    """
+    sql_select_hashed = (
+        f'SELECT * from {table}_meme WHERE {table}_meme.hash = '
+    )
+    sql_select_user = (
+        f'SELECT * from {table}_user WHERE {table}_user.user_id = '
+    )
+    sql_get_ids_by_id = f'SELECT id from {table}_meme ORDER BY id DESC'
+    sql_get_ids_by_ts = f'SELECT id from {table}_meme ORDER BY date DESC'
+    sql_insert_meme = f'INSERT INTO {table}_meme VALUES (?, ?, ?)'
+    sql_insert_user = f'INSERT INTO {table}_user VALUES (?, ?, ?)'
+    sql_delete_ids = f'DELETE from {table}_meme WHERE id = ?'
+    sql_delete_users = f'DELETE from {table}_user WHERE id = ?'
 
     queries = {
-        "create_memes": sql_create_memes_table,
-        "select_hashed": sql_select_hashed,
-        "get_ids_by_ids": sql_get_ids_by_id,
-        "get_ids_by_timestamp": sql_get_ids_by_ts,
-        "delete_ids": sql_delete_ids,
-        "insert": sql_insert,
+        'create_memes': sql_create_memes_table,
+        'create_users': sql_create_user_table,
+        'select_hashed': sql_select_hashed,
+        'select_user': sql_select_user,
+        'get_ids_by_ids': sql_get_ids_by_id,
+        'get_ids_by_timestamp': sql_get_ids_by_ts,
+        'delete_ids': sql_delete_ids,
+        'delete_users': sql_delete_users,
+        'insert_meme': sql_insert_meme,
+        'insert_user': sql_insert_user,
     }
     return queries[name]
